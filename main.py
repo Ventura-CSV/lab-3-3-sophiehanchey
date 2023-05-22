7c3b18ef95f4c7913214e006c38db363467bb513
Kyu Lee <kwooler@gmail.com>
2023-05-21 20:37:06 -0700

    Lab 3-3

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    num1 = int(input('Enter the first number: '))
+    num2 = int(input('Enter the second number: '))
+    num3 = int(input('Enter the third number: '))
+    """
+    Code Your Program here
+    """
+
+    print(f'The greates number is {maxnum}')
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return maxnum
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,15 +10,13 @@ def test_main_1():
     datastr = '10\n97\n45\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*97[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 97
 
 
 def test_main_2():
@@ -27,15 +25,13 @@ def test_main_2():
     datastr = '100\n50\n10'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*100[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 100
 
 
 def test_main_3():
@@ -44,12 +40,10 @@ def test_main_3():
     datastr = '1\n2\n3\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*3[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 3
